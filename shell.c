399de96133f2361e83aa66ab87517c05920e82f1
Gonzalo Carlo Cardenas Magno <gonzalocarlo.cardenasmagno@griffithuni.edu.au>
2015-03-27 17:58:29 +1000

    Second version of CLI. It executes arbitrary commands as a normal shell client using the system funtion

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 
 /* definition of functions */
 static char *read_line(char *line, int buffer);
+char ** parse_args();
 
 /* List of commands allowed to the client */
 char *cmd[1] = { "exit" };
@@ -17,7 +18,8 @@ char line[200];
  * @return int
  */
 int main(void)
-{       
+{   
+    char **margv;  
     int l;
     
     while(1) {
@@ -27,11 +29,11 @@ int main(void)
         /* Cleaning up break line from the input*/
         l = strlen(line);
         if (l == 0)
-                break;
+            break;
         if (line[--l] == '\n') {
-                if (l == 0)
-                    break;
-                line[l] = '\0';
+            if (l == 0)
+                continue;
+            line[l] = '\0';
         } else if (l == sizeof(line) - 2) {
             
             /* Prints error if command line is longer that 200 characters */
@@ -39,11 +41,15 @@ int main(void)
             break;
         }
         
+        /* Parsing line to get the command and args entered */
+        margv = parse_args();
+        
         /* Exits if line is equal to exit */
-        if(strcmp(line, cmd[0]) == 0)
+        if(strcmp(margv[0], cmd[0]) == 0)
             break;
-        fprintf(stdout,"%s\n",line);
-
+        
+        /* Executes arbitrary commands using the system function */
+         system(line);
     }
     return EXIT_SUCCESS;
 }
@@ -60,4 +66,35 @@ static char *read_line(char *line, int buffer)
     printf("CLI> ");
     fflush(stdout);
     return fgets(line, buffer, stdin);
+}
+
+/**
+ * Parses line entered into an array.
+ * 
+ * @return 
+ */
+char ** parse_args() {
+    static char *rargv[20];
+    char **argp;
+    const char *delim;
+    char tmpline[sizeof(line)];
+    
+    /* defines the delimiter for arguments */
+    delim = " ";
+    
+    /* Creates a copy of the read line */
+    strcpy(tmpline, line);
+    
+    argp = rargv;
+    
+    /* get the first token */
+    *argp = strtok(tmpline, delim);
+   
+    /* walk through other tokens */
+    while( *argp != NULL ) 
+    {
+        argp++;
+        *argp = strtok(NULL, tmpline);
+    }
+    return rargv;
 }
