9a12b9c47159477e1459b316e449821fee14ead8
Gonzalo Carlo Cardenas Magno <gonzalocarlo.cardenasmagno@griffithuni.edu.au>
2015-04-07 18:39:33 +1000

    First working version changing directories assignment1m5 steps 1 and 2

--- shell.c
+++ shell.c
@@ -10,10 +10,32 @@ static char *read_line(char *line, int buffer);
 char ** parse_args();
 int my_system(const char *command);
 
+void exit_(void);
+void cd(const char *path);
+
+struct cmd *getcmd(const char *name);
+
 extern int errno;
 
+/* table structure for extra commands on the mini-shell */
+struct cmd {
+    const char *c_name; // command name
+
+    /* 
+     * Handlers for command depending of nature of parameters. Values can be
+     * NULL; however, one of them must be non-NULL
+     */
+    void (*c_handler_v)(int, char **);
+    void (*c_handler_0)(void);
+    void (*c_handler_1)(const char *);
+};
+
 /* List of extra commands for the mini-shell */
-char *cmd[1] = { "exit" };
+struct cmd cmdtab[] = {
+    { "exit", NULL, exit_, NULL },
+    { "cd", NULL, NULL, cd },
+    { 0, 0, 0, 0 }
+};
 
 /* line to be read from the stdin */
 char line[200];
@@ -69,6 +91,7 @@ int my_system(const char *command)
     pid_t cpid;
     int cstatus;
     int wait_opt;
+    struct cmd *c;
     
     /* creates a copy of the command */
     tmpcmd = strdup(command);
@@ -84,8 +107,30 @@ int my_system(const char *command)
     margv = parse_args(tmpcmd);
     
     /* Exits if command line is equal to exit */
+/*
     if (strcmp(margv[0], cmd[0]) == 0)
         exit(EXIT_SUCCESS);
+*/
+    
+    /* 
+     * Searches command in the custom command lists first; if not found, continues
+     * execution with the execvp() function.
+     */
+    c = getcmd(margv[0]);
+    
+    /* If command exists on the list, executes handler. */
+    if (c != NULL ) {
+        if (c->c_handler_v) {
+            c->c_handler_v(*margv[0], &margv[0]);
+            return 0;
+        } else if (c->c_handler_0) {
+            c->c_handler_0();
+            return 0;
+        } else if (c->c_handler_1) {
+            c->c_handler_1(margv[1]);
+            return 0;
+        }
+    }
 
     /* Creates a child process to execute command */
     cpid = fork();
@@ -164,4 +209,36 @@ char ** parse_args(char *command)
         *argp = strtok(NULL, delim);
     }
     return rargv;
+}
+
+struct cmd *getcmd(const char *name)
+{
+    const char *p, *q;
+    struct cmd *c;
+    for (c = cmdtab; (p = c->c_name) != NULL; c++) {
+        for (q = name; *q == *p++; q++)
+                if (*q == 0)
+                        return (c);
+    }
+    return NULL;
+}
+
+/**
+ * Function that executes the 'exit command'
+ * @type command handler
+ */
+void exit_(void)
+{
+    exit(EXIT_SUCCESS);
+}
+
+/**
+ * Executes directory change for the shell
+ * @type command handler
+ * @param path
+ */
+void cd(const char *path)
+{
+    // executes chdir
+    chdir(path);
 }
