4388081e14c1c73b5828469e7af6fb50f37f1e82
Gonzalo Carlo Cardenas Magno <gonzalocarlo.cardenasmagno@griffithuni.edu.au>
2015-04-12 12:14:48 +1000

    Allowing multiple command execution separated by ';' and redesign of parse_args to better handle multiple task execution

--- shell.c
+++ shell.c
@@ -101,6 +101,7 @@ int my_system(const char *command)
     int redout = 0;
     int redin = 0;
     char *d = ">";
+    FILE *fp;
     
     memset(&tmp_argv[0], 0, sizeof(tmp_argv));
     
@@ -116,12 +117,6 @@ int my_system(const char *command)
 
     /* Parsing line to get the command and args entered */
     margv = parse_args(tmpcmd);
-    
-    /* Exits if command line is equal to exit */
-/*
-    if (strcmp(margv[0], cmd[0]) == 0)
-        exit(EXIT_SUCCESS);
-*/
     
     /* 
      * Searches command in the custom command lists first; if not found, continues
@@ -145,85 +140,91 @@ int my_system(const char *command)
     
     i = 0;
     
-/*
-    fprintf(stdout, "arrives: %s", margv[0]);
-    fprintf(stdout, "comparing: %d", strcmp(margv[0], d));
-    fprintf(stderr, "cat: %s\n", strerror( errno ));
-*/
-    
     tmp_argp = tmp_argv;
-    while (margv[i] != NULL) {
-        // fprintf(stdout, "fiel: %s", margv[i]);
+    while (1) {
+        if (margv[i] == NULL || !strcmp(margv[i], ";")) {
+            
+            /* Creates a child process to execute command */
+            cpid = fork();
+            if (cpid == -1) {
+                return -1;
+            } else if(cpid == 0) {
+
+                /* If process is a child */
+                if (redout) {
+                    fp = freopen(ofile, "w+", stdout);
+                }
+
+                if (redin) {
+                    fp = freopen(ifile, "r", stdin);
+                }
+
+                errno = 0;
+                execvp(tmp_argv[0], &tmp_argv[0]);
+                if (errno == ENOENT)
+                    fprintf(stderr, "%s: command not found\n", margv[0]);
+                else
+                    fprintf(stderr, "Unexpected error while executing command.\n");
+                exit(EXIT_FAILURE);
+            } else if (cpid > 0) {
+
+                /* Default: shell waits for children. */
+                wait_opt = 0;
+
+                /* If &, shell continues. */
+                if (amp) 
+                    wait_opt = WNOHANG;
+
+                if (waitpid(cpid, &cstatus, wait_opt) == -1)
+                    return -1;
+                else if (WIFEXITED(cstatus)) {
+                    if (margv[i] != NULL && !strcmp(margv[i], ";")) {
+                        memset(&tmp_argv[0], 0, sizeof(tmp_argv));
+                        tmp_argp = tmp_argv;
+                        i++;
+                        if (margv[i] == NULL) {
+                            return 0;
+                        }
+                        
+                        /* Resets the option for redirection of stdout & stdin */
+                        redout = 0;
+                        redin = 0;
+                        continue;
+                    }
+
+                    if (margv[i] == NULL) {
+                        return 0;
+                    }
+                
+                }
+
+                /* If child process is running on background means cstatus >= 0 */
+                if (amp && &cstatus != NULL && cstatus >= 0)
+                    return 0;
+
+                return -1;
+            }
+        }
+        
         if (!strcmp(margv[i], ">")) {
             ofile= strdup(margv[i + 1]);
-           // fprintf(stdout, "file: %s", ofile);
-            //tmp_argp++;
             redout = 1;
             *tmp_argp = NULL;
-            break;
+            i = i + 2;
+            continue;
         }
         
         if (!strcmp(margv[i], "<")) {
             ifile= strdup(margv[i + 1]);
             redin = 1;
             *tmp_argp = NULL;
-            break;
+            i = i + 2;
+            continue;
         }
         
-        *tmp_argp = margv[i];
+        *tmp_argp = strdup(margv[i]);
         tmp_argp++;
         i++;
-    }
-
-    *tmp_argp = NULL;
-    
-    /* Creates a child process to execute command */
-    cpid = fork();
-    if (cpid == -1) {
-        return -1;
-    } else if(cpid == 0) {
-        
-        /* If process is a child */
-        
-        
-
-        FILE *fp;
-        if (redout) {
-            fp = freopen(ofile, "w+", stdout);
-        }
-        
-        if (redin) {
-            fp = freopen(ifile, "r", stdin);
-        }
-
-        
-        errno = 0;
-        execvp(tmp_argv[0], &tmp_argv[0]);
-        
-        if (errno == ENOENT)
-            fprintf(stderr, "%s: command not found\n", margv[0]);
-        else
-            fprintf(stderr, "Unexpected error while executing command.\n");
-        exit(EXIT_FAILURE);
-    } else if (cpid > 0) {
-        
-        /* Default: shell waits for children. */
-        wait_opt = 0;
-        
-        /* If &, shell continues. */
-        if (amp) 
-            wait_opt = WNOHANG;
-            
-        if (waitpid(cpid, &cstatus, wait_opt) == -1)
-            return -1;
-        else if (WIFEXITED(cstatus)) 
-            return WEXITSTATUS(cstatus);
-        
-        /* If child process is running on background means cstatus >= 0 */
-        if (amp && &cstatus != NULL && cstatus >= 0)
-            return 0;
-        
-        return -1;
     }
 }
 
@@ -249,27 +250,110 @@ static char *read_line(char *line, int buffer)
  */
 char ** parse_args(char *command)
 {
-    static char *rargv[20];
+    static char *rargv[200];
     char **argp;
-    const char delim[] = " ";
-    char *tmpline;
-    
-    /* Creates a copy of the read line */
-    tmpline = strdup(command);
-
     argp = rargv;
-
-    /* get the first token */
-    *argp = strtok(tmpline, delim);
-       
-    /* walk through other tokens */
-    while (*argp != NULL) {
-        argp++;
-        *argp = strtok(NULL, delim);
+    
+    char word[200];
+    char *p;
+    char *pword;
+    char prev = ' ';
+    
+    memset(&rargv[0], 0, sizeof(rargv));
+    
+    pword = word;
+    argp = rargv;
+  
+    p = strdup(command);
+    int i = 0;
+    int j = 0;
+    int l = sizeof(line);
+    
+    while ( i < l) {
+        
+        switch(*p) {
+            case ' ' :
+                if (any(prev, " ;<>"))
+                    break;
+                *argp = strdup(word);
+                argp++;
+                memset(&word, 0, sizeof(word));
+                pword = word;
+                
+                break;
+            case ';' :
+                if (!any(prev, " ;<>")) {
+                    *argp = strdup(word);
+                    argp++;
+                    memset(&word, 0, sizeof(word));
+                    pword = word;
+                }
+                *argp = ";";
+                argp++;
+                break;
+            case '>' :
+                if (!any(prev, " ;<>")) {
+                    *argp = strdup(word);
+                    argp++;
+                    memset(&word, 0, sizeof(word));
+                    pword = word;
+                }
+                *argp = ">";
+                argp++;
+                break;
+                
+            case '<' :
+                if (!any(prev, " ;<>")) {
+                    *argp = strdup(word);
+                    argp++;
+                    memset(&word, 0, sizeof(word));
+                    pword = word;
+                }
+                *argp = "<";
+                argp++;
+                break;
+            case '"' :
+                break;
+            default:
+                *pword = *p;
+                pword++;
+                break;
+        }
+        prev = *p;
+        p++;
+        i++;
     }
+    if (pword > word){
+       *argp = strdup(word);
+    }
+    
     return rargv;
 }
 
+/**
+ * Checks if character c is in the string *as
+ * 
+ * @param c
+ * @param as
+ * @return 
+ */
+int any(int c, char *as)
+{
+    register char *s;
+    
+    s = as;
+    while(*s)
+        if(*s++ == c)
+            return(1);
+    return(0);
+}
+
+/**
+ * Resolves the handler of an specific command define  in cmdtab table.
+ * 
+ * @param name
+ * @return 
+ */
 struct cmd *getcmd(const char *name)
 {
     const char *p, *q;
